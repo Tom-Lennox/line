0a67428aefd1c246b5e956f82f32be1400c4c3b6
Tom-Lennox <dennkousekka1212@gmail.com>
2020-06-22 07:34:20 +0900

    dotenv導入

---
+++ 20200620_line_callback_bot/app.py
@@ -0,0 +1,20 @@
+from __future__ import unicode_literals
+from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+
+import configparser
+import random
+
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+load_dotenv(verbose=True)
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+print(os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]);
+print(os.environ["YOUR_CHANNEL_SECRET"]);
